87a9f085ea162bad75ab784de6f495d15917257a
GINTOAHC <chaotingchen10@gmail.com>
2023-11-29 11:49:26 +0800

    style: Styling security page

--- app/settings/Security.tsx
+++ app/settings/Security.tsx
@@ -56,7 +56,7 @@ export default function Account() {
           {<p className="text-blue-950  font-bold text-lg">E-Mail</p>}
           <section className="flex flex-row my-3 ">
             <section className=" w-80">
-              {<p className="text-blue-950  left-10 ">{email}</p>}
+              {<p className="text-blue-950 left-10">{email}</p>}
             </section>
             <section className="w-80" />
             <button
@@ -68,7 +68,7 @@ export default function Account() {
           </section>
         </section>
         <section className="">
-          {<p className="text-blue-950  font-bold text-lg mb-5">Password</p>}
+          {<p className="text-blue-950 font-bold text-lg mb-5">Password</p>}
           <div className=" pl-10 ">
             <section className="flex flex-row  my-3">
               <section className=" w-48">
@@ -81,16 +81,16 @@ export default function Account() {
                 onChange={(e) => setPassword(e.target.value)}
               />
               <button
-                className=" bg-transparent"
+                className="bg-transparent"
                 onClick={() => forgetPassword()}
               >
-                <p className=" ml-3 text-cyan-800  text-xs">Forget password?</p>
+                <p className="ml-3 text-cyan-800 text-xs">Forget password?</p>
               </button>
             </section>
 
-            <section className="flex flex-row  my-3">
+            <section className="flex flex-row my-3">
               <section className=" w-48">
-                {<p className="text-blue-950  ">New password</p>}
+                {<p className="text-blue-950">New password</p>}
               </section>
               <input
                 type="password"
@@ -100,8 +100,8 @@ export default function Account() {
               />
             </section>
 
-            <section className="flex flex-row  my-3 ">
-              <section className=" w-48">
+            <section className="flex flex-row my-3">
+              <section className="w-48">
                 {<p className="text-blue-950 ">Confirm new password</p>}
               </section>
               <input
@@ -186,36 +186,34 @@ export default function Account() {
             </div>
           </dialog>
 
-          <p className=" ml-8 mb text-gray-500   ">
-            {' '}
-            suspend your account until you sign back in{' '}
+          <p className="ml-8 mb text-gray-500">
+            suspend your account until you sign back in
           </p>
         </section>
 
         <section>
           <button
-            className=" bg-transparent"
+            className="bg-transparent"
             onClick={() =>
               (document.getElementById('deleteModal')! as any).showModal()
             }
           >
-            <p className=" my-4 text-rose-700"> Delete account </p>
+            <p className="my-4 text-rose-700"> Delete account </p>
           </button>
           <dialog id="deleteModal" className="modal">
-            <div className=" modal-box bg-white w-11/12 max-w-2xl h-96">
+            <div className="modal-box bg-white w-11/12 max-w-2xl h-96">
               <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
                 <button className="btn btn-md btn-circle btn-ghost absolute right-4 top-4 text-slate-400">
-                  <p className=" text-2xl">✕</p>
+                  <p className="text-2xl">✕</p>
                 </button>
               </form>
               <div className="p-12">
-                <p className=" mb-4 text-2xl text-red-600 flex">
-                  <BsExclamationCircle className=" self-center mr-2" /> Deleting
+                <p className="mb-4 text-2xl text-red-600 flex">
+                  <BsExclamationCircle className="self-center mr-2" /> Deleting
                   your account
                 </p>
-                <p className=" my-3 text-lg">
-                  {' '}
+                <p className="my-3 text-lg">
                   Your account will be removed from this website permanently,
                   including your profile, posts and content you saved.
                 </p>
@@ -223,7 +221,7 @@ export default function Account() {
                 <input
                   type="password"
                   placeholder="current password"
-                  className=" input input-bordered max-h-10 bg-transparent rounded border-slate-400 border-2"
+                  className="input input-bordered max-h-10 bg-transparent rounded border-slate-400 border-2"
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
@@ -234,7 +232,7 @@ export default function Account() {
                 }}
                 className="btn btn-sm btn-neutral absolute right-16 bottom-10 w-24 bg-red-600 rounded-3xl"
               >
-                <p className=" text-lg normal-case text-white font-normal ">
+                <p className="text-lg normal-case text-white font-normal">
                   Delete
                 </p>
               </button>
@@ -242,7 +240,7 @@ export default function Account() {
               <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
                 <button className="btn btn-sm btn-neutral absolute right-44 bottom-10 bg-white border-red-500 rounded-3xl">
-                  <p className=" text-lg normal-case text-red-500 font-medium ">
+                  <p className="text-lg normal-case text-red-500 font-medium">
                     Cancel
                   </p>
                 </button>
