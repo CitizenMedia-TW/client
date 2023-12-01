9d33ee6e1d39cb5a8cdc6d147246abf842512007
GINTOAHC <chaotingchen10@gmail.com>
2023-12-02 00:59:00 +0800

    fix: Fix story page error

--- app/stories/[storyid]/page.tsx
+++ app/stories/[storyid]/page.tsx
@@ -2,13 +2,6 @@
 'use client'
 import React from 'react'
 import { StoryServices } from '@/api/services'
-import { generateHTML } from '@tiptap/html'
-import StarterKit from '@tiptap/starter-kit'
-import Image from '@tiptap/extension-image'
-import parse from 'html-react-parser'
-
-// Use CSS Modules to style story body component
-import storyStyles from './story.module.css'
 
 import dynamic from 'next/dynamic'
 import { OutputData } from '@editorjs/editorjs'
@@ -28,80 +21,38 @@ interface Story {
   tags: string[]
 }
 
-export default function tmpFunction({
-  params,
-}: {
-  params: { storyid: string }
-}) {
+export default function Page({ params }: { params: { storyid: string } }) {
   const [data, setData] = React.useState<Story | null>(null)
-  const [content, setContent] = React.useState<OutputData>({
-    blocks: [
-      {
-        type: 'text',
-        data: 'Loading',
-      },
-    ],
-  })
+  const [content, setContent] = React.useState<OutputData | null>(null)
+  const [dataReady, setDataReady] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    StoryServices.getStoryById(params.storyid).then((res) => {
-      if (res && res.data != 'Story not found') setData(res.data)
-    })
+    async function fetchData() {
+      const res = await StoryServices.getStoryById(params.storyid)
+      if (res && res.data != 'Story not found') {
+        setData(res.data)
+        setContent(JSON.parse(res.data.content))
+        setDataReady(true)
+      }
+    }
+    fetchData()
   }, [params.storyid])
-
-  React.useEffect(() => {
-    setContent(JSON.parse(data?.content ?? '{}'))
-  }, [data?.content])
 
   return (
     <div>
-      <EditorBlock
-        data={content}
-        onDataChange={setContent}
-        holder="editorjs-container"
-        readOnly={true}
-      />
-    </div>
-  )
-}
-
-function Home({ params }: { params: { storyid: string } }) {
-  const [data, setData] = React.useState<Story | null>(null)
-  const [content, setContent] = React.useState<string | null>(null)
-
-  React.useEffect(() => {
-    StoryServices.getStoryById(params.storyid).then((res) => {
-      if (res && res.data != 'Story not found') setData(res.data)
-    })
-  }, [params.storyid])
-
-  React.useEffect(() => {
-    setContent(
-      generateHTML(
-        JSON.parse(
-          data?.content ??
-            '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Loading..."}]}]}'
-        ),
-        [StarterKit, Image]
-      )
-    )
-  }, [data?.content])
-
-  return (
-    <div className="min-h-screen">
-      <div>My Post: {params.storyid}</div>
-      <button onClick={() => console.log(params)}>Click</button>
-      {data ? (
-        <div className="m-12 border-black border-r-2 border-4">
-          <p>Title: {data && data.title}</p>
-          <p>Sub Title: {data && data.subTitle}</p>
-          <br />
-          <div className={storyStyles.content}>{content && parse(content)}</div>
-          <br />
+      {dataReady && content ? (
+        <div className="prose">
+          <h1>{data?.title}</h1>
+          <EditorBlock
+            data={content}
+            onDataChange={setContent}
+            holder="editorjs-container"
+            readOnly={true}
+          />
           <p>Tags: {data && data.tags.join(', ')}</p>
         </div>
       ) : (
-        <p>Story not found</p>
+        <p>Loading...</p>
       )}
     </div>
   )
