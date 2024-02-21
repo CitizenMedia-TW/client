0ce6ed160bd305df9edd404e055e0cb50ba99475
GNITOAHC <chaotingchen10@gmail.com>
2024-02-22 00:20:36 +0800

    settings page (#8)
    * feat: settings/account with following issue to solve: svg style, photo dialog, and link datas update after input not applied
    
    * feat: settings/notification with following work to do: check boxes style, checkbox onCheckedChange effect appling
    
    * feat: settings/workspace
    
    * feat: add component: checkbox, input, and switch
    
    * build: package.json for components dependencies
    
    ---------
    
    Co-authored-by: Paul1336 <paul.k.c1336@gmail.com>

--- app/settings/Workspace.tsx
+++ app/settings/Workspace.tsx
@@ -1,70 +1,68 @@
-import React from 'react'
-import { useTheme } from 'next-themes'
+import React from "react";
+import { useTheme } from "next-themes";
+import { Switch } from "@/components/ui/switch";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 export default function Workspace() {
-  const { theme, setTheme } = useTheme()
-  const [themeMode, setThemeMode] = React.useState(theme === 'system')
-  const [darkMode, setDarkMode] = React.useState(theme === 'dark')
+  const { theme, setTheme } = useTheme();
+  const [themeMode, setThemeMode] = React.useState(theme === "system");
+  const [darkMode, setDarkMode] = React.useState(theme === "dark");
 
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
 
-  if (!mounted) return <div>loading...</div>
+  if (!mounted) return <div>loading...</div>;
 
   const toggleThemeMode = () => {
     if (themeMode) {
-      setTheme('light')
-      setDarkMode(false)
+      setTheme("light");
+      setDarkMode(false);
     } else {
-      setTheme('system')
+      setTheme("system");
     }
-    setThemeMode(!themeMode)
-  }
+    setThemeMode(!themeMode);
+  };
   const toggleDarkMode = () => {
     if (darkMode) {
-      setTheme('light')
+      setTheme("light");
     } else {
-      setTheme('dark')
+      setTheme("dark");
     }
-    setDarkMode(!darkMode)
-  }
+    setDarkMode(!darkMode);
+  };
 
   return (
     <div>
-      <p>Workspace</p>
-      <div className="form-control">
-        <label className="label cursor-pointer">
-          <span className="label-text">System Default</span>
-          <input
-            type="checkbox"
-            className="toggle"
-            checked={themeMode}
-            onChange={() => toggleThemeMode()}
-          />
-        </label>
-        {!themeMode && (
-          <section>
-            <label className="label cursor-pointer">
-              <span className="label-text">Light Mode</span>
-              <input
-                type="checkbox"
-                className="toggle"
-                checked={!darkMode}
-                onChange={() => toggleDarkMode()}
-              />
-            </label>
-            <label className="label cursor-pointer">
-              <span className="label-text">Dark Mode</span>
-              <input
-                type="checkbox"
-                className="toggle"
-                checked={darkMode}
-                onChange={() => toggleDarkMode()}
-              />
-            </label>
-          </section>
-        )}
+      <div className="flex px-36 place-content-between">
+        <p className="text-2xl font-bold">System default</p>
+        <Switch
+          id=""
+          checked={themeMode}
+          onCheckedChange={() => toggleThemeMode()}
+        />
       </div>
+
+      {!themeMode && (
+        <section className="pt-8 pl-48">
+          <div className="flex gap-x-7 items-center">
+            <Checkbox
+              className="h-4 w-4 rounded-full"
+              checked={!darkMode}
+              onCheckedChange={() => toggleDarkMode()}
+            />
+            <p className="font-medium text-2xl">Dark</p>
+          </div>
+          <div className="flex pt-1.5 gap-x-7 items-center">
+            <Checkbox
+              className="h-4 w-4 rounded-full"
+              checked={darkMode}
+              onCheckedChange={() => toggleDarkMode()}
+            />
+            <p className="font-medium text-2xl">Light</p>
+          </div>
+        </section>
+      )}
     </div>
-  )
+  );
 }
