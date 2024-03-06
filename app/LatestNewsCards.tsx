83bef2e924b4d8710d10e3fbed08f9d08d11676c
Paul1336 <paul.k.c1336@gmail.com>
2024-03-07 00:22:41 +0800

    feat: Main page The latest news; refactor: Recommanded topic tags

--- app/LatestNewsCards.tsx
+++ app/LatestNewsCards.tsx
@@ -5,12 +5,48 @@ import { StoryServices } from '@/api/services'
 import Link from 'next/link'
 import { useTags } from './LatestNews'
 import Image from 'next/image'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
 
 interface Story {
   author: string
   title: string
   createdAt: Date
   _id: string
+}
+
+function CarouselOrientation() {
+  return (
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      orientation="vertical"
+      className="w-full max-w-xs"
+    >
+      <CarouselContent className="-mt-1 h-[200px]">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="pt-1 md:basis-1/2">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
 }
 
 export default function Page({ className }: { className: string }) {
@@ -37,52 +73,62 @@ export default function Page({ className }: { className: string }) {
   // TODO: Replace with real story's data
   return (
     <main className={className}>
-      <div className="carousel carousel-vertical flex w-full max-h-[calc(90%)]">
-        {stories &&
-          stories.map((data) => (
-            <div
-              key={`${data._id}`}
-              className="carousel-item mb-3 bg-white bg-opacity-50 rounded-md flex w-full h-[204px] justify-between"
-            >
-              <div className="flex flex-col">
-                <div className="flex flex-row items-center h-8">
-                  <Image
-                    unoptimized
-                    src={session?.user?.avatar as string}
-                    alt="here was a logo:("
-                    width={30}
-                    height={30}
-                    className="rounded-full w-8 h-8 border-solid border-black"
-                  />
-                  <p className="font-medium text-back">{data.author}</p>
-                </div>
-                <div className="mt-2 h-24">
-                  <Link href="/stories/[id]" as={`/stories/${data._id}`}>
-                    <h1 className="text-base font-bold">{data.title}</h1>
-                  </Link>
-                </div>
+      <Carousel orientation="vertical" className="h-full">
+        <div className="overflow-y-auto h-[180px] md:h-[525px]">
+          <CarouselContent className="">
+            <CarouselItem className="">
+              {stories &&
+                stories.map((data) => (
+                  <div
+                    key={`${data._id}`}
+                    className="pb-10 pl-4 mb-3 bg-white bg-opacity-50 rounded-md flex w-full h-56 justify-between"
+                  >
+                    <div className="flex flex-col w-40 sm:w-80">
+                      <div className="flex flex-row items-center h-12">
+                        <Image
+                          unoptimized
+                          src={session?.user?.avatar as string}
+                          alt="here was a logo:("
+                          width={96}
+                          height={96}
+                          className="rounded-full w-12 h-12 border-solid border-border"
+                        />
+                        <p className="pl-4 text-lg font-medium text-back">
+                          {data.author}
+                        </p>
+                      </div>
+                      <div className="mt-2 h-24">
+                        <Link href="/stories/[id]" as={`/stories/${data._id}`}>
+                          <h1 className="text-xl font-extrabold overflow-hidden">
+                            {data.title}
+                          </h1>
+                        </Link>
+                      </div>
 
-                <div className="flex flex-row m-1 gap-1">
-                  <p className="text-xs text-black">
-                    {new Date(data.createdAt).toLocaleDateString()}
-                  </p>
-                </div>
-              </div>
+                      <div className="flex flex-row m-1 gap-1">
+                        <p className="text-base font-light text-black">
+                          {new Date(data.createdAt).toLocaleDateString()}
+                        </p>
+                      </div>
+                    </div>
 
-              <div>
-                {/* <div className="round-md w-full h-full bg-[#D9D9D9]" /> */}
-                <Image
-                  src="https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg"
-                  alt="image"
-                  width={204}
-                  height={204}
-                  className="overflow-hidden"
-                  unoptimized
-                />
-              </div>
-            </div>
-          ))}
-      </div>
+                    <div className="w-32 sm:w-auto">
+                      {/* <div className="round-md w-full h-full bg-[#D9D9D9]" /> */}
+                      <Image
+                        src="https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg"
+                        alt="image"
+                        width={176}
+                        height={176}
+                        className="overflow-hidden"
+                        unoptimized
+                      />
+                    </div>
+                  </div>
+                ))}
+            </CarouselItem>
+          </CarouselContent>
+        </div>
+      </Carousel>
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { FiThumbsUp } from 'react-icons/fi'
 import LatestNewsIcon from '@/public/latest-news.svg'
 import Image from 'next/image'
 import LatestNews from './LatestNews'
-import RecommandedTag from './RecommandedTag'
 
 export default function Home() {
   return (
@@ -20,26 +19,12 @@ export default function Home() {
       </section>
       <hr className="h-px bg-border w-full" />
       <section className="flex flex-col w-full h-full">
-        <div className="flex items-center pl-5 pt-5 pb-12">
+        <div className="flex items-center pl-5 pt-5 sm:pb-12">
           <Image src={LatestNewsIcon} alt="" className="h-4" />
           <p className="pl-2 font-semibold text-lg">The latest news</p>
         </div>
         <div className="flex flex-col sm:flex-row h-full">
-          <div className="w-full sm:w-1/2 pr-6 pl-16">
-            <LatestNews />
-          </div>
-          <div className="w-full sm:w-1/2 h-full flex pt-2 pl-6 pr-7">
-            <div className="w-full h-full border-x border-border">
-              <p className="pl-6 font-medium text-lg text-slate-400">
-                Recommanded Topic
-              </p>
-              <div className="w-full h-64 px-12 pt-8 pb-8">
-                <RecommandedTag />
-              </div>
-              <hr className="mx-5 h-px bg-border" />
-              <div className="w-full"></div>
-            </div>
-          </div>
+          <LatestNews />
         </div>
       </section>
       {/* <LatestNews /> */}
