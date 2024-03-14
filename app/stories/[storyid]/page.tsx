6f899714d238a7e83c2d148c40b935a8dbc7b630
Paul1336 <paul.k.c1336@gmail.com>
2024-03-14 23:23:36 +0800

    feat: comment, copy from the branch, notification. The change hadn't been tested, do not merge before it's tested

--- app/stories/[storyid]/page.tsx
+++ app/stories/[storyid]/page.tsx
@@ -2,10 +2,14 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
 import { StoryServices, UserServices } from '@/api/services'
 import { Skeleton } from '@/components/ui/skeleton'
 import { PrimarySection, SectionContent } from '@/components/PrimarySection'
 import { Loading } from '@/components/Loading'
+import { Button } from '@/components/ui/button'
+
+import Comments from './comments'
 
 import dynamic from 'next/dynamic'
 import { OutputData } from '@editorjs/editorjs'
@@ -14,6 +18,7 @@ const EditorBlock = dynamic(() => import('@/app/components/editor/Editor'), {
 })
 
 type Story = {
+  _id: string
   author: string
   authorId: string
   content: string
@@ -72,6 +77,16 @@ function AuthorSection({ data }: { data: Story }) {
 }
 
 export default function Page({ params }: { params: { storyid: string } }) {
+  const [isShow, setShow] = React.useState<string>('hidden')
+
+  const handle_showMore_click = () => {
+    if (isShow == 'hidden') {
+      setShow('')
+    } else {
+      setShow('hidden')
+    }
+  }
+
   const [data, setData] = React.useState<Story | null>(null)
   const [content, setContent] = React.useState<OutputData | null>(null)
   const [dataReady, setDataReady] = React.useState<boolean>(false)
@@ -79,9 +94,9 @@ export default function Page({ params }: { params: { storyid: string } }) {
   React.useEffect(() => {
     async function fetchData() {
       const res = await StoryServices.getStoryById(params.storyid)
-      if (res && res.status == 200) {
-        setData(res.data.story)
-        setContent(JSON.parse(res.data.story.content))
+      if (res && res.data != 'Story not found') {
+        setData(res.data)
+        setContent(JSON.parse(res.data.content))
         setDataReady(true)
       }
     }
@@ -89,41 +104,53 @@ export default function Page({ params }: { params: { storyid: string } }) {
   }, [params.storyid])
 
   return (
-    <div className="flex flex-col md:flex-row px-10">
-      <section className="basis-3/4">
-        <div className="h-9" />
-        {dataReady && content ? (
-          <div className="">
-            <h1>{data?.title}</h1>
-            <EditorBlock
-              data={content}
-              onDataChange={setContent}
-              holder="editorjs-container"
-              readOnly={true}
-            />
-            <p>Tags: {data && data.tags.join(', ')}</p>
-            <div className="h-9" />
-          </div>
-        ) : (
-          <Loading />
-        )}
-        <section className="section">
-          <p className="section-title">Comments</p>
-          {/* <Comments /> */}
-        </section>
-      </section>
-      <div className="h-9 block md:hidden" />
-      <section className="basis-1/4">
-        <div className="h-9" />
-        <section className="section">
-          {dataReady && data ? (
-            // <p>{data.author}</p>
-            <AuthorSection data={data} />
+    <div className="w-full">
+      <div className="flex flex-col md:flex-row px-10">
+        <section className="basis-3/4">
+          <div className="h-9" />
+          {dataReady && content ? (
+            <div className="">
+              <h1>{data?.title}</h1>
+              <EditorBlock
+                data={content}
+                onDataChange={setContent}
+                holder="editorjs-container"
+                readOnly={true}
+              />
+              <p>Tags: {data && data.tags.join(', ')}</p>
+              <div className="h-9" />
+            </div>
           ) : (
-            <span className="loading loading-spinner" />
+            <Loading />
           )}
+          <section className="section">
+            <p className="section-title">Comments</p>
+            <Button
+              className="border-2 border-slate-400 bg-background text-slate-600 rounded-3xl hover:bg-slate-200"
+              onClick={handle_showMore_click}
+            >
+              Show more
+            </Button>
+            {/* <Comments/> */}
+          </section>
         </section>
-      </section>
+        <div className="h-9 block md:hidden" />
+
+        <section className="basis-1/4">
+          <div className="h-9" />
+          <section className="section">
+            {dataReady && data ? (
+              // <p>{data.author}</p>
+              <AuthorSection data={data} />
+            ) : (
+              <span className="loading loading-spinner" />
+            )}
+          </section>
+        </section>
+      </div>
+      <div className={`fixed bottom-6 -right-[720px] ${isShow}`}>
+        <Comments isShow={isShow} setShow={setShow} />
+      </div>
     </div>
   )
 }
