e76d82ea708276c697af08b6242f0ab2bf4281ec
Paul1336 <paul.k.c1336@gmail.com>
2024-03-19 14:42:26 +0800

    feat: LatestNews. fix: Main page layout and number of stories returned by story service.

--- app/LatestNewsTags.tsx
+++ app/LatestNewsTags.tsx
@@ -1,13 +1,23 @@
 'use client'
 import React from 'react'
 import { useTags } from './LatestNews'
+import { Button } from '@/components/ui/button'
 
 export default function Page({ className }: { className: string }) {
   const { tags, setTags } = useTags()
-
   return (
     <main className={className}>
-      <p>LatestNewsTags</p>
+      {tags.map((tag) => (
+        <Button
+          key={tag}
+          className={`h-9 bg-border rounded-full px-5 flex items-center ${
+            tag === '' ? 'w-20 lg:w-32 hidden md:flex' : 'w-fit'
+          }`}
+          style={{ whiteSpace: 'nowrap' }}
+        >
+          <p className="text-xl font-light">{tag}</p>
+        </Button>
+      ))}
     </main>
   )
 }
