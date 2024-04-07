ee76ff6a055baefb761f80560f79d662e64a3860
Paul1336 <paul.k.c1336@gmail.com>
2024-04-07 17:59:32 +0800

    fix: connect to backend data

--- app/user/Library.tsx
+++ app/user/Library.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 export default function Library({ className }: { className: string }) {
   return (
-    <div className={className}>
-      <h1>Library</h1>
-    </div>
+    <ScrollArea className="h-[960px] w-full rounded-md my-4">
+      <div className="p-4">
+        <h1>Library</h1>
+      </div>
+    </ScrollArea>
   )
 }
--- app/user/Stories.tsx
+++ app/user/Stories.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { StoryServices } from '@/api/services'
 import { useSession } from 'next-auth/react'
 import { Story } from '@/api/services/story-services'
-
+import { ScrollArea } from '@/components/ui/scroll-area'
 export default function Stories({ className }: { className: string }) {
   const { data: session } = useSession()
   const [data, setData] = React.useState<Story[] | null>(null)
@@ -23,19 +23,47 @@ export default function Stories({ className }: { className: string }) {
   if (!session.user) return <div>loading...</div>
 
   return (
-    <div className={`${className} overscroll-auto`}>
-      <h1>Stories</h1>
-      {data &&
-        data.map((story) => (
-          <Link
-            href={`/stories/${story.id}`}
-            key={story.id}
-            className="card border-2"
-          >
-            <div className="card-title">{story.title}</div>
-            <p>{story.subTitle}</p>
-          </Link>
-        ))}
-    </div>
+    <ScrollArea className="w-full rounded-md my-4">
+      <div className="">
+        {data &&
+          data.map((story) => (
+            <Link
+              href={`/stories/${story.id}`}
+              key={story.id}
+              className="card border-b-4 pt-10 px-6 pb-4 h-72"
+            >
+              <div className="font-extrabold text-3xl">{story.title}</div>
+              <div className="flex">
+                <div className="w-2/3">
+                  <div className="my-4 flex flex-row w-full gap-4">
+                    <div className="flex flex-col gap-2">
+                      <div className="font-medium text-lg">
+                        {story.subTitle}
+                      </div>
+                      <div className="font-medium text-lg w-4/6 overflow-hidden">
+                        story:
+                        fwefwkmrgwkgmwkomqkefm,koqmfomqk,wfklmkjmrgwjkoqelpwfoqefj
+                      </div>
+                      <div className="my-2 flex flex-row gap-3"></div>
+                    </div>
+                  </div>
+                  <div className="flex gap-x-3">
+                    {story.tags &&
+                      story.tags.map((tag, tagIndex) => (
+                        <div
+                          key={tagIndex}
+                          className="grow-0 rounded-3xl bg-gray-200 hover:bg-gray-100 hover:-translate-y-1"
+                        >
+                          <div className="text-xs m-2">{tag}</div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+                <div className="w-6/12 h-36 bg-gray-300 p-4"></div>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </ScrollArea>
   )
 }
