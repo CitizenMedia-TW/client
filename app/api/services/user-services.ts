f6db83a83c29e40a58a35b152778aac360bea446
GINTOAHC <chaotingchen10@gmail.com>
2024-05-17 20:42:33 +0800

    fix: Fix user-service & account page

--- app/api/services/user-services.ts
+++ app/api/services/user-services.ts
@@ -3,25 +3,23 @@ const BROKER_SERVICE_URL =
   process.env.BROKER_SERVICE_URL || 'http://localhost:8080'
 
 class UserServices {
-  async getProfileLinks(jwtToken: string) {
-    console.log(jwtToken)
-    if (!jwtToken) return { data: ['No token'] }
-    return await axios.get(`${BROKER_SERVICE_URL}/user/profile-links`, {
-      headers: { Authorization: jwtToken },
-    })
+  async getProfileLinks(mail: string) {
+    return await axios.get(`${BROKER_SERVICE_URL}/user/profile-links/${mail}`)
   }
   async getPublicUser(id: string) {
     return await axios.get(`${BROKER_SERVICE_URL}/user/public-profile/${id}`)
   }
-  async modifyProfileLinks(jwtToken: string, modifiedLink: string){
+  async modifyProfileLinks(jwtToken: string, modifiedLink: string) {
     if (!jwtToken) return { data: ['No token'] }
-    console.log("modified link", modifiedLink)
-    return await axios.patch(`${BROKER_SERVICE_URL}/user/profile-links`,
-    {
-      modify: modifiedLink
-    }, {
-      headers: { Authorization: jwtToken },
-    })
+    return await axios.patch(
+      `${BROKER_SERVICE_URL}/user/profile-links`,
+      {
+        modify: modifiedLink,
+      },
+      {
+        headers: { Authorization: jwtToken },
+      }
+    )
   }
 }
 
