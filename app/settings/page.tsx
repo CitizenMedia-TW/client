63106c5663c423f46413313ee93f3f77ecde9f36
raamiiChu <ericchu0617@gmail.com>
2024-05-24 22:05:40 +0800

    style(settings): rwd

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -67,56 +67,56 @@ export default function Page() {
   }, [searchParams])
 
   return (
+    <main className="pt-24 px-4 sm:px-8 md:px-12 lg:px-24 pb-12">
+      <section role="grid" className="grid grid-cols-12 gap-y-4">
+        <h1 className="col-span-6 sm:col-span-full text-5xl font-bold">
+          Settings
+        </h1>
+        {/* Show on @media (min-width: 640px) */}
+        <section className="col-span-full sm:flex flex-row space-x-4 transition-transform duration-100 hidden">
+          {Object.keys(subPages).map((key) => (
+            <Button
+              key={key}
+              variant={subPages[key].activate ? 'outline' : 'ghost'}
+              onClick={() => {
+                console.log(pathname + '?' + 'tab=' + key)
+                router.push(pathname + '?' + 'tab=' + key)
+              }}
+            >
+              {key}
+            </Button>
+          ))}
+        </section>
+        {/* Hidden on @media (max-width: 640px) */}
 
-    <main className="h-full pt-24 px-4 sm:px-12 md:px-24 pb-12">
-
-      <p className="text-5xl font-bold ">Settings</p>
-      {/* Show on @media (min-width: 640px) */}
-      <section className="sm:flex flex-row m-4 space-x-4 transition-transform duration-100 hidden">
-        {Object.keys(subPages).map((key) => (
-          <Button
-            key={key}
-            variant={subPages[key].activate ? 'outline' : 'ghost'}
-            onClick={() => {
-              console.log(pathname + '?' + 'tab=' + key)
-              router.push(pathname + '?' + 'tab=' + key)
+        <section className="col-span-6 sm:hidden flex justify-end mt-auto">
+          <Select
+            onValueChange={(val) => {
+              console.log(pathname + '?' + 'tab=' + val)
+              router.push(pathname + '?' + 'tab=' + val)
             }}
           >
-            {key}
-          </Button>
-        ))}
+            <SelectTrigger className="w-24">
+              <SelectValue placeholder="Account" defaultValue={'account'} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="Account">Account</SelectItem>
+                <SelectItem value="Security">Security</SelectItem>
+                <SelectItem value="Notification">Notification</SelectItem>
+                <SelectItem value="Workspace">Workspace</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </section>
+
+        <hr className="col-span-full h-1 bg-primary mt-5" />
+        <div className="col-span-full py-4">
+          {Object.keys(subPages).map(
+            (key) => subPages[key].activate && subPages[key].component(key)
+          )}
+        </div>
       </section>
-      {/* Hidden on @media (max-width: 640px) */}
-
-      <section className="sm:hidden flex justify-end sm:justify-center">
-        <Select
-          onValueChange={(val) => {
-            console.log(pathname + '?' + 'tab=' + val)
-            router.push(pathname + '?' + 'tab=' + val)
-          }}
-        >
-
-          <SelectTrigger className="w-24">
-            <SelectValue placeholder="Account" defaultValue={'account'} />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="Account">Account</SelectItem>
-              <SelectItem value="Security">Security</SelectItem>
-              <SelectItem value="Notification">Notification</SelectItem>
-              <SelectItem value="Workspace">Workspace</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </section>
-
-      <hr className="h-[3px] bg-primary mt-5" />
-      <div className="h-1/5 overflow-hidden pt-12">
-
-        {Object.keys(subPages).map(
-          (key) => subPages[key].activate && subPages[key].component(key)
-        )}
-      </div>
     </main>
   )
 }
