3b70d262c394daf8379f40a5cca11e8770def480
raamiiChu <ericchu0617@gmail.com>
2024-05-26 16:51:30 +0800

    style(settings?tab=Notification): rwd, adopt shadcn/label

--- app/settings/Notification.tsx
+++ app/settings/Notification.tsx
@@ -1,79 +1,143 @@
 import React from 'react'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Switch } from '@/components/ui/switch'
+import { Label } from '@radix-ui/react-label'
 
 export default function Notification() {
   return (
+    <form className="px-4 lg:px-12 space-y-16">
+      <section className="flex justify-between items-center">
+        <Label htmlFor="pauseAll" className="text-2xl font-bold cursor-pointer">
+          Pause all
+        </Label>
+        <Switch id="pauseAll" />
+      </section>
 
-    <div className="w-full h-screen px-4 sm:px-12">
-      <div className="flex place-content-between">
+      <ul className="space-y-12 sm:px-4 lg:px-24">
+        <li className="space-y-4">
+          <h2 className="font-bold text-2xl">Comments</h2>
 
-        <p className="text-2xl font-bold">Pause all</p>
-        <Switch id="" className="mr-2" />
-      </div>
-
-      <div className="w-full flex flex-col gap-y-12 pr-4 mt-12 px-4 sm:px-12">
-        <div className="pl-12">
-          <p className="font-bold text-2xl">Comments</p>
-
-          <div className="flex flex-col gap-y-7 pl-10 pt-5">
-
-            <div className="flex place-content-between">
-              <p className="font-normal text-xl">New comments on your post</p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+          <section className="space-y-8 sm:pl-10">
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="newCommentsOnYourPost"
+                className="font-normal text-xl cursor-pointer"
+              >
+                New comments on your post
+              </Label>
+              <Checkbox
+                id="newCommentsOnYourPost"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-            <div className="flex  place-content-between">
-              <p className="font-normal text-xl">
+            <div className="flex  justify-between items-center gap-4">
+              <Label
+                htmlFor="newCommentsOnPostYouLiked"
+                className="font-normal text-xl cursor-pointer"
+              >
                 New comments on post you liked
-              </p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+              </Label>
+              <Checkbox
+                id="newCommentsOnPostYouLiked"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-          </div>
-        </div>
-        <div className="pl-12">
-          <p className="font-bold text-2xl">Likes</p>
-          <div className="flex flex-col gap-y-7 pl-10 pt-5">
-            <div className="flex place-content-between">
-              <p className="font-normal text-xl">Likes on your post</p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+          </section>
+        </li>
+
+        <li className="space-y-4">
+          <h2 className="font-bold text-2xl">Likes</h2>
+
+          <section className="space-y-8 sm:pl-10">
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="likesOnYourPost"
+                className="font-normal text-xl cursor-pointer"
+              >
+                Likes on your post
+              </Label>
+              <Checkbox
+                id="likesOnYourPost"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-            <div className="flex place-content-between">
-              <p className="font-normal text-xl">Likes on post you liked</p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="Likes on post you liked"
+                className="font-normal text-xl cursor-pointer"
+              >
+                Likes on post you liked
+              </Label>
+              <Checkbox
+                id="LikesOnPostYouLiked"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-          </div>
-        </div>
-        <div className="pl-12">
-          <p className="font-bold text-2xl">Stories</p>
-          <div className="flex flex-col gap-y-7 pl-10 pt-5">
-            <div className="flex  place-content-between">
-              <p className="font-normal text-xl">
+          </section>
+        </li>
+
+        <li className="space-y-4">
+          <h2 className="font-bold text-2xl">Stories</h2>
+
+          <section className="space-y-8 sm:pl-10">
+            <div className="flex  justify-between items-center gap-4">
+              <Label
+                htmlFor="newStoriesFromWriters"
+                className="font-normal text-xl cursor-pointer"
+              >
                 New stories from writers you are following
-              </p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+              </Label>
+              <Checkbox
+                id="newStoriesFromWriters"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-            <div className="flex  place-content-between">
-              <p className="font-normal text-xl">
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="newStoriesFromTags"
+                className="font-normal text-xl cursor-pointer"
+              >
                 New stories from tags you are following
-              </p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+              </Label>
+              <Checkbox
+                id="newStoriesFromTags"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-          </div>
-        </div>
-        <div className="pl-12">
-          <p className="font-bold text-2xl">Follows</p>
-          <div className="flex flex-col gap-y-7 pl-10 pt-5">
-            <div className="flex place-content-between">
-              <p className="font-normal text-xl">New follower</p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+          </section>
+        </li>
+
+        <li className="space-y-4">
+          <h2 className="font-bold text-2xl">Follows</h2>
+
+          <section className="space-y-8 sm:pl-10">
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="newFollower"
+                className="font-normal text-xl cursor-pointer"
+              >
+                New follower
+              </Label>
+              <Checkbox
+                id="newFollower"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-            <div className="flex place-content-between">
-              <p className="font-normal text-xl">Forward post</p>
-              <Checkbox className="h-7 w-7 rounded-md" />
+            <div className="flex justify-between items-center gap-4">
+              <Label
+                htmlFor="forwardPost"
+                className="font-normal text-xl cursor-pointer"
+              >
+                Forward post
+              </Label>
+              <Checkbox
+                id="forwardPost"
+                className="size-8 rounded-md transition-all"
+              />
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
+          </section>
+        </li>
+      </ul>
+    </form>
   )
 }
