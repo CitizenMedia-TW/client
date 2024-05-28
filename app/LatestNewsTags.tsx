561d397614e7165fcc4bdce7eda4bef02a75244e
raamiiChu <ericchu0617@gmail.com>
2024-05-28 23:18:51 +0800

    feat(homepage/LatestNewsTags): show tags

--- app/LatestNewsTags.tsx
+++ app/LatestNewsTags.tsx
@@ -1,13 +1,22 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useTagsContext } from '@/context/TagsContext'
 
 export default function Page({ className }: { className: string }) {
   const { tags, setTags } = useTagsContext()
 
+  useEffect(() => {
+    console.log(tags)
+  }, [tags])
+
   return (
     <main className={className}>
       <p>LatestNewsTags</p>
+      <ul>
+        {tags.map((tag, index) => {
+          return <li key={index}>{tag}</li>
+        })}
+      </ul>
     </main>
   )
 }
