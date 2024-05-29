1b56285174dc9457842ed77caaf3a82f40227e96
raamiiChu <ericchu0617@gmail.com>
2024-05-29 17:53:15 +0800

    docs: add stories types

--- types/stories.ts
+++ types/stories.ts
@@ -11,3 +11,30 @@ export type StoryPreview = {
   createdAt: createdTime
   tags: string[]
 }
+
+type newStoryData = {
+  authorId: string
+  content: string
+  title: string
+  subTitle: string
+  tags: string[]
+}
+
+export type Comment = {
+  id: string
+  content: string
+  commenter: string
+  commenterId: string
+}
+
+export type Story = {
+  id: string // Not given by story-service
+  author: string
+  authorId: string
+  content: string
+  comments: Comment[]
+  title: string
+  subTitle: string
+  createdAt: createdTime
+  tags: string[]
+}
