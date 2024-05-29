046c3362f4b4084f89c82a4f37a9d13d36c442ab
raamiiChu <ericchu0617@gmail.com>
2024-05-29 16:01:06 +0800

    feat(homepage/LatestNews): add ChosenTagsProvider

--- app/LatestNews.tsx
+++ app/LatestNews.tsx
@@ -4,14 +4,17 @@ import LatestNewsCards from './LatestNewsCards'
 import LatestNewsTags from './LatestNewsTags'
 
 import { TagsProvider } from '@/context/TagsContext'
+import { ChosenTagsProvider } from '@/context/ChosenTagsContext'
 
 export default function Page() {
   return (
-    <TagsProvider>
-      <main className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:col-rows-1">
-        <LatestNewsCards className="row-start-2 md:row-start-1 p-2" />
-        <LatestNewsTags className="row-start-1 p-2" />
-      </main>
-    </TagsProvider>
+    <ChosenTagsProvider>
+      <TagsProvider>
+        <main className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:col-rows-1">
+          <LatestNewsCards className="row-start-2 md:row-start-1 p-2" />
+          <LatestNewsTags className="row-start-1 p-2" />
+        </main>
+      </TagsProvider>
+    </ChosenTagsProvider>
   )
 }
