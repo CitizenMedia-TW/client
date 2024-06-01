6366759071798319b7d31249a09bb39bf56bb288
raamiiChu <ericchu0617@gmail.com>
2024-06-01 18:35:52 +0800

    style(homepage): modify css styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MainCarousel from './MainCarousel'
-import { ThumbsUp } from 'lucide-react'
+import { ThumbsUp, MessageSquare } from 'lucide-react'
 
 import LatestNewsIcon from '@/public/latest-news.svg'
 import Image from 'next/image'
@@ -8,23 +8,24 @@ import LatestNews from './LatestNews'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 md:p-16">
-      <div className="flex flex-col w-full p-2">
-        <div className="flex flex-row items-center">
+    <main className="min-h-lvh flex flex-col items-center py-8">
+      <article className="flex flex-col w-full gap-y-8">
+        <section className="flex items-center gap-x-2 px-8">
           <ThumbsUp />
-          <p className="pl-2">Official Recommendation</p>
-        </div>
-        <MainCarousel />
-      </div>
-      <div className="flex flex-col w-full shadow-blue">
-        <div className="flex flex-row items-center mx-5 my-2">
-          <Image src={LatestNewsIcon} alt="" className="h-4" />
-          <p className="pl-2">The latest news</p>
-        </div>
-        <section className="w-full">
-          <LatestNews />
+          <h2 className="text-2xl font-bold">Official Recommendation</h2>
         </section>
-      </div>
+
+        <MainCarousel />
+      </article>
+
+      <article className="w-full flex flex-col py-4 lg:p-16 gap-y-2 lg:gap-y-8 shadow-blue">
+        <section className="flex items-center gap-x-2 px-2 lg:px-0">
+          <MessageSquare />
+          <h2 className="text-2xl font-bold">The Latest News</h2>
+        </section>
+
+        <LatestNews />
+      </article>
       {/* <LatestNews /> */}
       {/* <MostViewed /> */}
       {/* <TraceAuthor /> */}
