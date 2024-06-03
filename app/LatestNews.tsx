171fcbfff2cf4ea39d2941a36e7c2bdc6dd18139
raamiiChu <ericchu0617@gmail.com>
2024-06-03 17:11:46 +0800

    style(homepage/LatestNews): fix bg color error

--- app/LatestNews.tsx
+++ app/LatestNews.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <TagsProvider>
         <main className="relative flex flex-col-reverse md:flex-row gap-4">
           <LatestNewsCards className="w-full md:w-1/2 p-4" />
-          <LatestNewsTags className="sticky top-[4.75rem] h-fit w-full md:w-1/2 px-4 lg:p-4 space-y-8 md:border-x-2 border-slate-200 bg-white shadow-xl md:shadow-none" />
+          <LatestNewsTags className="sticky top-[4.75rem] h-fit w-full md:w-1/2 px-4 lg:p-4 space-y-8 md:border-x-2 border-slate-200 bg-background shadow-xl md:shadow-none" />
         </main>
       </TagsProvider>
     </ChosenTagsProvider>
