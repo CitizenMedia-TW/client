06cd781d7aef20105809c825f63d274501e0101d
raamiiChu <ericchu0617@gmail.com>
2024-06-24 16:58:13 +0800

    fix(LatestNews): types of components

--- app/components/homepage/LatestNews.tsx
+++ app/components/homepage/LatestNews.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import LatestNewsCards from './LatestNewsCards'
 import LatestNewsTags from './LatestNewsTags'
--- app/components/homepage/LatestNewsCards.tsx
+++ app/components/homepage/LatestNewsCards.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 
