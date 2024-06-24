fc41a8b67ee537f2c62348c527b567a5c6a1c5de
raamiiChu <ericchu0617@gmail.com>
2024-06-24 16:59:18 +0800

    refactor: data fetching ( server side )

--- app/components/homepage/MainCarousel.tsx
+++ app/components/homepage/MainCarousel.tsx
@@ -28,62 +28,29 @@ import { toDate } from '@/lib/utils'
 
 import MainCarouselSkeleton from './MainCarouselSkeleton'
 
-// const stories = [
-//   {
-//     id: '48f60ce3-1bb7-4f83-9614-dc8ee5647403',
-//     author: 'dev',
-//     title: 'First Story',
-//     subTitle: 'First story from dev',
-//     createdAt: { seconds: 1716875096, nanos: 658239000 },
-//     tags: ['tag-1', 'tag-2', 'tag-3'],
-//   },
-//   {
-//     id: '48f60ce3-1bb7-4f84-9614-dc8ee5647403',
-//     author: 'dev 2',
-//     title:
-//       'Second Story 123 213 123 123 1 231 23 123 12 31 23 12 3 123 12 3 123 1 23 12 31 23 12 3 12 3 12 31 23 1 3 1 23 12 31 23 1 3 12 312',
-//     subTitle: 'Second story from dev 2',
-//     createdAt: { seconds: 1716875096, nanos: 658239000 },
-//     tags: ['tag-2', 'tag-4', 'tag-5'],
-//   },
-//   {
-//     id: '48f60ce3-1bb7-4f85-9614-dc8ee5647403',
-//     author: 'dev 3',
-//     title: 'third Story',
-//     subTitle: 'third story from dev 3',
-//     createdAt: { seconds: 1716875096, nanos: 658239000 },
-//     tags: ['tag-2', 'tag-1', 'tag-5'],
-//   },
-//   {
-//     id: '48f60ce3-1bb7-4f85-9614-dc8ee5647403',
-//     author: 'dev 3',
-//     title: 'third Story',
-//     subTitle: 'third story from dev 3',
-//     createdAt: { seconds: 1716875096, nanos: 658239000 },
-//     tags: ['tag-2', 'tag-1', 'tag-5'],
-//   },
-//   {
-//     id: '48f60ce3-1bb7-4f85-9614-dc8ee5647403',
-//     author: 'dev 3',
-//     title: 'third Story',
-//     subTitle: 'third story from dev 3',
-//     createdAt: { seconds: 1716875096, nanos: 658239000 },
-//     tags: ['tag-2', 'tag-1', 'tag-5'],
-//   },
-// ]
+import { Story } from '@/types/stories'
 
-export default function MainCarousel() {
+type MainCarouselProps = { stories: Story[] | null }
+
+export default function MainCarousel({ stories }: MainCarouselProps) {
   const { data: session } = useSession()
   const [login, setLogin] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
-  const {
-    data: stories,
-    error,
-    isLoading,
-  } = useSWR('getLatestStories', async () => {
-    const stories = await StoryServices.getCarouselStories()
-    return stories
-  })
+  // const {
+  //   data: stories,
+  //   error,
+  //   isLoading,
+  // } = useSWR('getLatestStories', async () => {
+  //   const stories = await StoryServices.getCarouselStories()
+  //   return stories
+  // })
+
+  useEffect(() => {
+    if (stories) {
+      setIsLoading(false)
+    }
+  }, [stories])
 
   useEffect(() => {
     if (session) setLogin(true)
