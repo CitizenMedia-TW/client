30fe627bce4e51d00c07dd5e2f5db7e7135bedc6
raamiiChu <ericchu0617@gmail.com>
2024-07-06 00:50:22 +0800

    refactor: export components

--- app/components/homepage/index.tsx
+++ app/components/homepage/index.tsx
@@ -1,4 +1,15 @@
 import LatestNews from './LatestNews'
+import LatestNewsCards from './LatestNewsCards'
+import LatestNewsCardsSkeleton from './LatestNewsCardsSkeleton'
+import LatestNewsTags from './LatestNewsTags'
 import MainCarousel from './MainCarousel'
+import MainCarouselSkeleton from './MainCarouselSkeleton'
 
-export { LatestNews, MainCarousel }
+export {
+  LatestNews,
+  LatestNewsCards,
+  LatestNewsCardsSkeleton,
+  LatestNewsTags,
+  MainCarousel,
+  MainCarouselSkeleton,
+}
